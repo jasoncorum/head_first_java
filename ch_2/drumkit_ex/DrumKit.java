6e9e30ee4f3680bf78fe67d2c9dcd4456d3181a8
Jason Corum <jason.corum@gmail.com>
2015-01-30 14:28:21 -0500

    Completes DrumKit exercise

---
+++ ch_2/drumkit_ex/DrumKit.java
@@ -0,0 +1,14 @@
+class DrumKit {
+
+	boolean topHat = true;
+	boolean snare = true;
+
+	void playSnare() {
+		System.out.println("Bang Bang Ba-bang!");
+	}
+
+	void playTopHat() {
+		System.out.println("Ding Ding Da-ding");
+	}
+
+}
